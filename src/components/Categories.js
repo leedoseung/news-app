8bdc87b6b2f3a156896608a5d2e9e46ace44e59b
leedooseung <leedoseu@gmail.com>
2019-12-11 16:06:37 +0900

    categories.js 수정

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -70,7 +70,7 @@ const Category = styled(NavLink)`
   }
 `;
 
-const Categories = ({ onSelect, category }) => {
+const Categories = () => {
   return (
     <CategoriesBlock>
       {categories.map(c => (
